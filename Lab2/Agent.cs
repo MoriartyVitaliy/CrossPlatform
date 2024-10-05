465585340426b87661b8a5491839e5f509f5f933
MoriartyVitaliy <vgriss@knu.ua>
2024-10-05 15:51:01 +0300

    Add Agent class to lab2

---
+++ Lab2/Agent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Agent
+    {
+        public Agent(int age, int risk)
+        {
+            Age = age;
+            Risk = risk;
+        }
+        public int Age { get; set; }
+        public int Risk { get; set; }
+    }
+}
