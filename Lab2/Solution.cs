4234fac1c3f32ad8563224ef2454b919f865a4e1
MoriartyVitaliy <vgriss@knu.ua>
2024-10-05 15:52:22 +0300

    Add finale solution for lab2

---
+++ Lab2/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Solution
+    {
+        public static int MinValue(int n, Agent[] agents, int[] minRisks)
+        {
+            if (n > 2)
+            {
+                minRisks[2] = agents[1].Risk + agents[2].Risk;
+            }
+            if (n > 3)
+            {
+                minRisks[3] = agents[1].Risk + agents[3].Risk;
+            }
+            for (int i = 4; i < n; i++)
+            {
+                minRisks[i] = Math.Min(minRisks[i - 2] + agents[i].Risk, minRisks[i - 3] + agents[i - 1].Risk + agents[i].Risk);
+            }
+
+            return minRisks[n - 1];
+        }
+    }
+}
