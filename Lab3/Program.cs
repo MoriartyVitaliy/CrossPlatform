b5cbf65c37bb5944ed093b07323b3a1c70695980
MoriartyVitaliy <vgriss@knu.ua>
2024-10-10 00:31:42 +0300

    Main program file of lab3

---
+++ Lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab3
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\INPUT.txt");
+            string outputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\OUTPUT.txt");
+
+            // Чтение входных данных
+            var (n, particles, destructionMatrix) = InputReader.ReadInput(inputFilePath);
+
+            // Поиск финальных состояний
+            var finalStates = ParticleProcessor.ProcessParticles(n, particles, destructionMatrix);
+
+            // Запись выходных данных
+            OutputWriter.WriteOutput(outputFilePath, finalStates);
+        }
+    }
+
+}
